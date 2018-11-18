793dfa7b1a609152b25d80e5fd23ac173fafd18c
rasoulpoordelan <rasoulpoordelan@gmail.com>
2018-11-18 10:56:21 +0330

    init

---
+++ src/main/java/com/onetechone/jenkin/jenkin/JenkinApplication.java
@@ -0,0 +1,12 @@
+package com.onetechone.jenkin.jenkin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinApplication.class, args);
+	}
+}
---
+++ src/test/java/com/onetechone/jenkin/jenkin/JenkinApplicationTests.java
@@ -0,0 +1,16 @@
+package com.onetechone.jenkin.jenkin;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JenkinApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
